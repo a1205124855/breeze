716a0bb0a4843a81d3b4124bbcb01fb364bfe671
booomu <33300600+booomu@users.noreply.github.com>
2019-02-26 07:58:19 +0800

    Create goflyway_linux_amd64.sh

---
+++ goflyway_linux_amd64.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#服务端:
+yum install wget
+mkdir -p /root/goflyway
+cd /root/goflyway
+wget https://github.com/coyove/goflyway/releases/download/2.0.0rc1/goflyway_linux_amd64.tar.gz
+tar -zxvf goflyway_linux_amd64.tar.gz
+
+
+'
