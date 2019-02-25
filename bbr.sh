9c4063a7e8451f767f4b5ad0014204107253376f
booomu <33300600+booomu@users.noreply.github.com>
2019-02-25 13:14:48 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-   echo 'net.ipv4.ip_forward=1' | tee -a /etc/sysctl.conf
    echo 'net.core.default_qdisc=fq' |  tee -a /etc/sysctl.conf
    echo 'net.ipv4.tcp_congestion_control=bbr' |  tee -a /etc/sysctl.conf
    sysctl -p
