08f6ab933b71fc181678b557d9e8dbb42f732b39
booomu <33300600+booomu@users.noreply.github.com>
2019-02-26 08:02:31 +0800

    Update goflyway_linux_amd64.sh

--- goflyway_linux_amd64.sh
+++ goflyway_linux_amd64.sh
@@ -5,6 +5,3 @@ mkdir -p /root/goflyway
 cd /root/goflyway
 wget https://github.com/coyove/goflyway/releases/download/2.0.0rc1/goflyway_linux_amd64.tar.gz
 tar -zxvf goflyway_linux_amd64.tar.gz
-
-
-'
